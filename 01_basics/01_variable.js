c19edb91d910608ee4ca0724d8c5059d11168b00
CodeByJatin123 <jatinverma45674@gmail.com>
2026-01-31 04:18:37 +0000

    updated

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=14453
+let account_email="abc@gmail.com"
+var accountpassword="123"
+accountcity="Jaipur"
+let accountstate;  //undefined
+
+
+account_email="hbc@gmail.com"
+accountpassword="8932"
+accountcity="Delhi"
+// accountId=3982  not allowed
+console.log(accountId);
+console.table([accountId,account_email,accountpassword,accountcity,accountstate]);
+
