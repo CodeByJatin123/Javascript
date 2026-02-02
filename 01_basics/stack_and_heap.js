cce76721ee30082dffd4a2a1aa8d7ddb481792a3
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-02 16:44:07 +0000

    stack and heap

---
+++ 01_basics/stack_and_heap.js
@@ -0,0 +1,5 @@
+let myyoutubename="Hitesh Choudhary";
+let anothername=myyoutubename;  //<<<<<<---not real value this is tthe copy of myyoutubename.
+anothername="Code with harry";  //<<<<<<--- real value is changed 
+console.log(myyoutubename);
+console.log(anothername);
