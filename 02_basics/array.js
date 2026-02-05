892c804d47b3b6fe25451cbcb35d0f9dd22e54c1
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-05 13:27:28 +0000

    update

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,5 +1,5 @@
 // array
-const myarr=[0,1,2,3,4,5];
+// const myarr=[0,1,2,3,4,5];
 // console.log(myarr[0]);
 
 //array methoods
@@ -19,13 +19,26 @@ const myarr=[0,1,2,3,4,5];
 // console.log(typeof newarr);  // string 
 
 //slice method
-console.log("A",myarr);
+// console.log("A",myarr);
 
-const myn1=myarr.slice(1,3);
-console.log(myn1);
-console.log("B",myarr); // original array is not changed..|
+// const myn1=myarr.slice(1,3);
+// console.log(myn1);
+// console.log("B",myarr); // original array is not changed..|
 
-//splice method
-const myn2=myarr.splice(1,3);
-console.log(myn2); // original array is changed..;
-console.log("C",myarr); 
+// //splice method
+// const myn2=myarr.splice(1,3);
+// console.log(myn2); 
+// console.log("C",myarr); // original array is changed..;
+
+// console.log(Array.isArray("Hitesh")); // to check whether it is array or not..
+// console.log(Array.from("hitesh"));
+// console.log(Array.from({name:"hitesh"})); // they give me intresting string.. but it is not array.. it is object.. so we can not use array method on it..
+
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3)); // this is another way of creating array.
+
+
+
