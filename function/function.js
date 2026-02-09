01b8a8cc2cdeb175b48a3608f29d0e2afeffa880
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-09 13:08:46 +0000

    function

---
+++ function/function.js
@@ -0,0 +1,22 @@
+// console.log("jatin");
+
+
+// function print(){
+//     console.log("Jatin")
+// }
+// print();
+
+// function add(num1,num2){
+//     return num1+num2;
+//     console.log("Hello"); // they does not working because they came after the return statement.
+// }
+// // add(2,4);
+// const result=add(2,2);
+// // console.log("Result:",result);
+
+// function justlogin(username){
+//     return `${username} just logged in!`;
+// }
+
+// console.log(justlogin("Jatin"));
+
