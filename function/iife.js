7c53b9abd2c3e3ba1167c118ff526bc5ae604efd
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-11 11:52:11 +0000

    iife

---
+++ function/iife.js
@@ -0,0 +1,9 @@
+// immediately invoked fnuction expression(IIFE)
+
+// (function name (){
+//     console.log("I am Jatin");
+// })();
+
+((name)=>{
+    console.log(`DB coneected to ${name}`);
+})('Hitesh')
