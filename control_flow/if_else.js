823a01a97e0d0b7f4d6441ebc73b1cc30b9e4b87
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-13 09:57:42 +0000

    if else update

--- control_flow/if_else.js
+++ control_flow/if_else.js
@@ -1,10 +1,10 @@
-// const age=18;
-// if(age>=18){
-//     console.log("You can drive!");
-// }
-// else{
-//     console.log("You can't drive!");   
-// }
+// // const age=18;
+// // if(age>=18){
+// //     console.log("You can drive!");
+// // }
+// // else{
+// //     console.log("You can't drive!");   
+// // }
 
 // const score=200;
 // if(score>100){
@@ -14,26 +14,26 @@
 // console.log(`User Power: ${power} `);
 
 
-// const balance=1200;
+// // const balance=1200;
 
-// if(balance<500){
-//     console.log("Balance less than 500");    
-// }
-// else if (balance<900){
-//     console.log("Balance less than 900");    
-// }
-// else{
-//     console.log("Balance is Greater than 1200");    
-// }
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
+// // if(balance<500){
+// //     console.log("Balance less than 500");    
+// // }
+// // else if (balance<900){
+// //     console.log("Balance less than 900");    
+// // }
+// // else{
+// //     console.log("Balance is Greater than 1200");    
+// // }
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
 
-if (userLoggedIn && debitCard && 2==3) {
-    console.log("Allow to buy course");
-}
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
 
-if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("User logged in");
-}
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
