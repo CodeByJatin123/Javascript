143b9372279fce43dd9dd9c97ef06bd4752caab9
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-14 13:52:37 +0000

    loops

---
+++ 05_itrations/for_loop.js
@@ -0,0 +1,21 @@
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+
+// for(let i=0;i<=10;i++){
+//     console.log(`Outer loop value ${i}`);
+//     for(let j=0;j<=10;j++){
+//         // console.log(`Inner loop value ${j}`);
+//         // console.log(i + '*' + j+'='+i*j);
+//         console.log(`${i}*${j}=${i*j}`);  //<< this is modren way...
+        
+//     }
+// }
+
+
+// let myarray=["Tea","Grrentea","Masala tea"];
+// console.log(myarray.length);
+// for(let i=0;i<myarray.length;i++){
+//     console.log(myarray[i]);   
+// }
