3130f1f289b7d56f26546f1510137c15ce4d749b
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-15 10:19:48 +0000

    while and do while

---
+++ 05_itrations/while_and_dowhile.js
@@ -0,0 +1,18 @@
+// let myarray=["Hero","Spederman","Batman"];
+
+
+// let arr=0;
+// while(arr<myarray.length){
+//     console.log(`value is ${myarray[arr]}`);  
+//     arr++;
+// }
+
+// // for(let i=0;i<myarray.length;i++){
+// //     console.log(`${myarray[i]}`);
+// // }
+
+let score=1;
+do{
+console.log(`${score}`);
+score++;
+}while(score<=10);
