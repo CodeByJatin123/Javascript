034bb70780c6d71cbcb56a19e52acde432a14801
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-15 15:11:11 +0000

    maps

--- 05_itrations/array_loop.js
+++ 05_itrations/array_loop.js
@@ -1,5 +1,24 @@
-let myarray=[1,2,3,4,5,6,7,8,9,10];
-for (const num of myarray) {
-    console.log(num);
+//higher array loop...
+
+
+// let myarray=[1,2,3,4,5,6,7,8,9,10];
+// for (const num of myarray) {
+//     console.log(num);
     
+// }
+
+// let name="jatin";
+// for (const num of name) {
+// console.log(num);   
+// }
+
+//maps...
+const map=new Map();
+map.set("IN","India");
+map.set("UN","United State");
+map.set("FR","France");
+map.set("IN","India");    // no Reapet in maps they are unique
+for (const [key,value] of map) {   // destructuring of array
+    
+    console.log(key,"->",value);
 }
