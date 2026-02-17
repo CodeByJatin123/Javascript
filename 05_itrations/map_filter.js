1736af5c6a64844cff3c5a0ad36e6d9c8a8cd664
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-17 14:34:29 +0000

    filter maps

---
+++ 05_itrations/map_filter.js
@@ -0,0 +1,14 @@
+// const coding=["JavaScript","Python","Java","C++","Go"];
+
+// const value=coding.forEach((itmes)=>{
+//     console.log(itmes);
+//     return itmes;
+// })
+// console.log(value);
+
+const mynums=[1,2,3,4,5,6,7,8,9,10];
+
+// const newnums=mynums.filter((num)=>{num>5});
+const newnums=mynums.filter((num)=>num>5);
+
+console.log(newnums);
