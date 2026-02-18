98489a1f915979d82e8a35ea4f6d2cc1ada81bdb
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-18 13:50:01 +0000

    problem add+10

---
+++ 05_itrations/seven.js
@@ -0,0 +1,13 @@
+const mynums=[1,2,3,4,5,6,7,8,9,10];
+
+const newnum=mynums.map((num)=>num+10)
+
+// console.log(newnum);
+
+// mynums.forEach((num)=>{  // with the help of for each loop
+//     num=num+10;
+//     console.log(num);
+    
+// })
+
+
