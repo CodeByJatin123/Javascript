5c121dbc889cc6f0fed994849b25c8338a61fabf
CodeByJatin123 <jatinverma45674@gmail.com>
2026-02-21 18:25:30 +0000

    protype

---
+++ prototype/proty[e.js
@@ -0,0 +1,38 @@
+//Prototype = ek object jisse dusre objects properties inherit karte hain.
+
+const prithviraj={
+    name:"prithviraj",
+    gernation:"grandfather",
+    cooktraditionaldish(){
+        return `${this.name} is cooking traditional dish`;
+    },
+};
+
+// console.log(prithviraj);
+
+const raj=Object.create(prithviraj);
+raj.name="raj";
+raj.gernation="father";
+raj.buisness= function(){
+    return `${this.name} is doing a buisnees`;
+}
+
+
+const ranbir=Object.create(raj);
+ranbir.name="ranbir";
+ranbir.gernation="son";
+ranbir.makerfilm=function(){
+    return `${this.name} is making a film`;
+}
+
+console.log(ranbir.cooktraditionaldish());
+console.log(ranbir.buisness());
+console.log(ranbir.makerfilm());
+
+
+Array.prototype.last=function(){    // making new protype metthod last
+    return this[this.length-1];
+}
+
+const arr=[1,2,3,4,5];
+console.log(arr.last());
